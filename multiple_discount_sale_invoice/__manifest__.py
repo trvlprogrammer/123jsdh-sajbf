6bc638b8b1d68e43ade34ccdd20293eeaf26a8c6
ridho <alfatihridhont@gmail.com>
2019-07-11 11:52:12 +0700

    [first commit multiple discount module]

---
+++ multiple_discount_sale_invoice/__manifest__.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Multiple Discount For Sale & Invoice",
+
+    'summary': """
+        Multiple discount in sale and invoicing""",
+
+    'description': """
+        Thank you for buying this module.
+        This module inherit sale and account to add new feature called multiple discount.
+        If you need customization odoo you can email me at alfatihridhont@gmail.com
+    """,
+
+    'author': "Alfatih Ridho NT",
+    'website': "alfatihridhont@gmail.com",
+    'category': 'Sale',
+    'license': 'OPL-1',
+    'price' : '40',
+    'currency' : 'EUR',
+    'version' : '11.0.1',
+    'support' : 'alfatihridhont@gmail.com',
+    'depends': ['base','sale_management'],
+    'data': [
+        'views/views.xml',
+        'views/templates.xml',
+    ],
+    'demo': [
+    ],
+}
