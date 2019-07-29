165f3bb9f716ab449aa57b490f4599035d7a727e
ridho <alfatihridhont@gmail.com>
2019-07-29 13:39:59 +0700

    [customer reviews]

---
+++ customer_reviews/tests/test_customer_reviews.py
@@ -0,0 +1,33 @@
+from odoo.tests import common
+ 
+ 
+class TestExecuteQuery(common.TransactionCase):
+    
+    def test_execute_query(self):
+        
+        
+        customer_object             = self.env['res.users'].browse(1).partner_id
+        customer_id                 = customer_object.id
+        customer_object.customer    = True
+        
+        vendor_obj = self.env['res.partner'].search([('supplier','=',True)])
+        vendor_id  = vendor_obj[0].id 
+        
+        data = {}
+        data['customer_id'] = customer_id
+        data['vendor_id']   = vendor_id
+        data['message']     = 'Very Good'
+        data['repetition']  = '6hours'
+        data['ratings']     = '5'
+        
+        record = self.env['customer.reviews'].create(data)
+        self.assertEqual(record.vendor_id.id, vendor_id)
+        self.assertEqual(record.ratings, vendor_obj[0].average_ratings)
+        self.assertEqual(vendor_obj[0].number_of_rates, 1)
+        
+        print("+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++")
+        print("Test Was Successfull")
+        
+        
+        
+        
